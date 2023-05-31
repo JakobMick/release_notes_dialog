f478a914ab0bb205a72267ce10a638da5f5bdb5e
Jakob Mick <jakobmick@live.de>
2023-06-01 01:07:27 +0200

    feat: add alert dialog parameters

--- lib/src/release_notes_dialog.dart
+++ lib/src/release_notes_dialog.dart
@@ -16,12 +16,14 @@ class ReleaseNotesDialog extends StatelessWidget {
     this.title = 'Release Notes',
     this.bullet = '•',
     this.closeButtonString,
-    this.backgroundColor,
-    this.elevation,
-    this.shape,
-    this.semanticLabel = 'Release Notes',
     this.titlePadding,
     this.contentPadding,
+    this.actionsPadding,
+    this.actionsAlignment,
+    this.actionsOverflowAlignment,
+    this.actionsOverflowDirection,
+    this.actionsOverflowButtonSpacing,
+    this.buttonPadding,
     this.bulletSpacing = 2.5,
     this.releaseSpacing = 12.5,
     this.releaseTitleSpacing = 12.5,
@@ -33,6 +35,16 @@ class ReleaseNotesDialog extends StatelessWidget {
     this.sublistTitleTextStyle,
     this.changeTextStyle,
     this.closeButtonTextStyle,
+    this.backgroundColor,
+    this.elevation,
+    this.shadowColor,
+    this.surfaceTintColor,
+    this.alignment,
+    this.insetPadding =
+        const EdgeInsets.symmetric(horizontal: 40.0, vertical: 24.0),
+    this.clipBehavior = Clip.none,
+    this.shape,
+    this.semanticLabel = 'Release Notes',
   }) : super(key: key);
 
   /// {@macro rnd.widget.releases}
@@ -55,8 +67,23 @@ class ReleaseNotesDialog extends StatelessWidget {
   /// {@macro flutter.material.dialog.elevation}
   final double? elevation;
 
+  /// {@macro flutter.material.dialog.shadowColor}
+  final Color? shadowColor;
+
+  /// {@macro flutter.material.dialog.surfaceTintColor}
+  final Color? surfaceTintColor;
+
   /// {@macro flutter.material.dialog.shape}
   final ShapeBorder? shape;
+
+  /// {@macro flutter.material.dialog.alignment}
+  final AlignmentGeometry? alignment;
+
+  /// {@macro flutter.material.dialog.insetPadding}
+  final EdgeInsets insetPadding;
+
+  /// {@macro flutter.material.dialog.clipBehavior}
+  final Clip clipBehavior;
 
   /// The semantic label of the dialog used by accessibility frameworks to
   /// announce screen transitions when the dialog is opened and closed.
@@ -70,14 +97,78 @@ class ReleaseNotesDialog extends StatelessWidget {
   final String semanticLabel;
 
   /// The padding around the dialogs title.
-  ///
-  /// Defaults to const EdgeInsets.fromLTRB(24.0, 24.0, 24.0, 0.0).
   final EdgeInsetsGeometry? titlePadding;
 
   /// The padding around the dialogs content.
-  ///
-  /// Defaults to const EdgeInsets.fromLTRB(24.0, 24.0, 24.0, 0.0).
   final EdgeInsetsGeometry? contentPadding;
+
+  /// The padding around the dialogs actions.
+  final EdgeInsetsGeometry? actionsPadding;
+
+  /// Defines the horizontal layout of the [actions] according to the same
+  /// rules as for [Row.mainAxisAlignment].
+  ///
+  /// This parameter is passed along to the dialog's [OverflowBar].
+  ///
+  /// If this parameter is null (the default) then [MainAxisAlignment.end]
+  /// is used.
+  final MainAxisAlignment? actionsAlignment;
+
+  /// The horizontal alignment of [actions] within the vertical
+  /// "overflow" layout.
+  ///
+  /// If the dialog's [actions] do not fit into a single row, then they
+  /// are arranged in a column. This parameter controls the horizontal
+  /// alignment of widgets in the case of an overflow.
+  ///
+  /// If this parameter is null (the default) then [OverflowBarAlignment.end]
+  /// is used.
+  ///
+  /// See also:
+  ///
+  /// * [OverflowBar], which [actions] configures to lay itself out.
+  final OverflowBarAlignment? actionsOverflowAlignment;
+
+  /// The vertical direction of [actions] if the children overflow
+  /// horizontally.
+  ///
+  /// If the dialog's [actions] do not fit into a single row, then they
+  /// are arranged in a column. The first action is at the top of the
+  /// column if this property is set to [VerticalDirection.down], since it
+  /// "starts" at the top and "ends" at the bottom. On the other hand,
+  /// the first action will be at the bottom of the column if this
+  /// property is set to [VerticalDirection.up], since it "starts" at the
+  /// bottom and "ends" at the top.
+  ///
+  /// See also:
+  ///
+  /// * [OverflowBar], which [actions] configures to lay itself out.
+  final VerticalDirection? actionsOverflowDirection;
+
+  /// The spacing between [actions] when the [OverflowBar] switches to a column
+  /// layout because the actions don't fit horizontally.
+  ///
+  /// If the widgets in [actions] do not fit into a single row, they are
+  /// arranged into a column. This parameter provides additional vertical space
+  /// between buttons when it does overflow.
+  ///
+  /// The button spacing may appear to be more than the value provided. This is
+  /// because most buttons adhere to the [MaterialTapTargetSize] of 48px. So,
+  /// even though a button might visually be 36px in height, it might still take
+  /// up to 48px vertically.
+  ///
+  /// If null then no spacing will be added in between buttons in an overflow
+  /// state.
+  final double? actionsOverflowButtonSpacing;
+
+  /// The padding that surrounds each button in [actions].
+  ///
+  /// This is different from [actionsPadding], which defines the padding
+  /// between the entire button bar and the edges of the dialog.
+  ///
+  /// If this property is null, then it will default to
+  /// 8.0 logical pixels on the left and right.
+  final EdgeInsetsGeometry? buttonPadding;
 
   /// {@macro rnd.widget.bulletSpacing}
   final double bulletSpacing;
@@ -126,9 +217,19 @@ class ReleaseNotesDialog extends StatelessWidget {
       titleTextStyle: titleTextStyle,
       contentPadding: contentPadding,
       contentTextStyle: changeTextStyle,
+      actionsPadding: actionsPadding,
+      actionsAlignment: actionsAlignment,
+      actionsOverflowAlignment: actionsOverflowAlignment,
+      actionsOverflowDirection: actionsOverflowDirection,
+      actionsOverflowButtonSpacing: actionsOverflowButtonSpacing,
+      buttonPadding: buttonPadding,
       backgroundColor: backgroundColor,
-      shape: shape,
       elevation: elevation,
+      shadowColor: shadowColor,
+      surfaceTintColor: surfaceTintColor,
+      insetPadding: insetPadding,
+      clipBehavior: clipBehavior,
+      alignment: alignment,
       semanticLabel: semanticLabel,
       content: ReleaseNotesWidget(
         releases: releases,
