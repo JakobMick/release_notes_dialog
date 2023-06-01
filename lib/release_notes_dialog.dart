346a980fcdd84af3d802fc5de5a00e301c3d677a
Jakob Mick <jakobmick@live.de>
2023-06-01 15:19:49 +0200

    feat: add release notes page

--- lib/release_notes_dialog.dart
+++ lib/release_notes_dialog.dart
@@ -4,4 +4,5 @@ export 'src/release.dart';
 export 'src/release_sublist.dart';
 
 export 'src/release_notes_dialog.dart';
+export 'src/release_notes_page.dart';
 export 'src/release_notes_widget.dart';
---
+++ lib/src/release_notes_page.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:release_notes_dialog/src/release_notes_widget.dart';
+import 'package:release_notes_dialog/src/release.dart';
+
+class ReleaseNotesPage extends StatelessWidget {
+  const ReleaseNotesPage({
+    Key? key,
+    required this.releases,
+    this.title = "Release Notes",
+    this.bullet,
+    this.bulletSpacing,
+    this.releaseSpacing,
+    this.releaseTitleSpacing,
+    this.sublistSpacing,
+    this.sublistTitleSpacing,
+    this.changeSpacing,
+    this.titleTextStyle,
+    this.releaseTitleTextStyle,
+    this.sublistTitleTextStyle,
+    this.changeTextStyle,
+  }) : super(key: key);
+
+  /// {@macro rnd.widget.releases}
+  final List<Release> releases;
+
+  /// The title of the [ReleaseNotesPage].
+  ///
+  /// Defaults to 'Release Notes'.
+  final String title;
+
+  /// {@macro rnd.widget.bullet}
+  final String? bullet;
+
+  /// {@macro rnd.widget.bulletSpacing}
+  final double? bulletSpacing;
+
+  /// {@macro rnd.widget.releaseSpacing}
+  final double? releaseSpacing;
+
+  /// {@macro rnd.widget.releaseTitleSpacing}
+  final double? releaseTitleSpacing;
+
+  /// {@macro rnd.widget.sublistSpacing}
+  final double? sublistSpacing;
+
+  /// {@macro rnd.widget.sublistTitleSpacing}
+  final double? sublistTitleSpacing;
+
+  /// {@macro rnd.widget.changeSpacing}
+  final double? changeSpacing;
+
+  /// The [TextStyle] for the dialogs title.
+  final TextStyle? titleTextStyle;
+
+  /// {@macro rnd.widget.releaseTitleTextStyle}
+  final TextStyle? releaseTitleTextStyle;
+
+  /// {@macro rnd.widget.sublistTitleTextStyle}
+  final TextStyle? sublistTitleTextStyle;
+
+  /// {@macro rnd.widget.changeTextStyle}
+  final TextStyle? changeTextStyle;
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Container(
+          constraints: BoxConstraints.loose(const Size.fromWidth(600.0)),
+          child: ListView(
+            primary: true,
+            padding: theme.useMaterial3
+                ? const EdgeInsetsDirectional.only(
+                    start: 16.0, end: 24.0, top: 8.0)
+                : const EdgeInsets.only(left: 16.0, right: 16.0, top: 8.0),
+            children: [
+              ReleaseNotesWidget(
+                releases: releases,
+                bullet: bullet,
+                bulletSpacing: bulletSpacing,
+                releaseSpacing: releaseSpacing,
+                releaseTitleSpacing: releaseTitleSpacing,
+                sublistSpacing: sublistSpacing,
+                sublistTitleSpacing: sublistTitleSpacing,
+                changeSpacing: changeSpacing,
+                releaseTitleTextStyle: releaseTitleTextStyle,
+                sublistTitleTextStyle: sublistTitleTextStyle,
+                changeTextStyle: changeTextStyle,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
